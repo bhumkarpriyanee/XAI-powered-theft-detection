7f4eb77e0021ee699e218a0e0a142346743332f0
Himani78116 <himanibagale@gmail.com>
2026-04-30 22:03:31 +0530

    Added dbatabase columns and fastapi in backend

--- backend/db.py
+++ backend/db.py
@@ -2,3 +2,27 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./data.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class MeterData(Base):
+    __tablename__ = "meter_data"
+
+    id = Column(Integer, primary_key=True, index = True)
+
+    customer_id = Column(Integer, index=True)
+
+    timestamp = Column(String)
+
+    voltage = Column(Float)
+
+    current = Column(Float)
+
+    power = Column(Float)
+    
---
+++ backend/main.py
@@ -0,0 +1,2 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
