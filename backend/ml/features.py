58dc208c0e0eb49adfb56d70ccd56300a8f414cc
Himani78116 <himanibagale@gmail.com>
2026-05-03 19:59:10 +0530

    Added features file for model

---
+++ backend/ml/features.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+from datetime import datetime
+
+def add_features(df: pd.DataFrame) -> pd.DataFrame:
+    df["ts"] = pd.to_datetime(df["timestamp"])
+
+    df["hour"] = df["ts"].dt.hour
+
+    df["dayofweek"] = df["ts"].dt.dayofweek
+
+    df.sort_values(["customer_id","ts"],inplace=True)
+
+    for window in [12,24,48]:
+        df[f"power_mean_{window}h"] = df.groupby("customer_id")["power"].transform( 
+            lambda x:
+            x.rolling(window,min_periods=1).mean()
+            )
+        
+        df["power_std_24h"] = df.groupby("customer_id")["power"].transform(
+            lambda x: x.rolling(24, min_periods = 3).std()
+        )
+
+        df["power_std_24h"] = df.groupby("customer_id")["power"].transform(
+            lambda x: x.rolling(24, min_periods=3).min()
+        )
+
+        df["power_max_24h"] = df.groupby("customer_id")["power"].transform(
+            lambda x: x.rolling(24,min_periods=3).max()
+         )
+        
+        return df
