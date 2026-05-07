f4df4b2dc5d32797d7cf045a2327d9a156767599
Himani78116 <himanibagale@gmail.com>
2026-05-07 22:56:35 +0530

    Added features.py to create columns for multiple readings

--- backend/ml/features.py
+++ backend/ml/features.py
@@ -11,22 +11,21 @@ def add_features(df: pd.DataFrame) -> pd.DataFrame:
 
     df.sort_values(["customer_id","ts"],inplace=True)
 
-    for window in [12,24,48]:
-        df[f"power_mean_{window}h"] = df.groupby("customer_id")["power"].transform( 
-            lambda x:
-            x.rolling(window,min_periods=1).mean()
-            )
-        
-        df["power_std_24h"] = df.groupby("customer_id")["power"].transform(
-            lambda x: x.rolling(24, min_periods = 3).std()
+    for window in [12, 24, 48]:
+        df[f"power_mean_{window}h"] = df.groupby("customer_id")["power"].transform(
+            lambda x: x.rolling(window, min_periods=1).mean()
         )
 
-        df["power_std_24h"] = df.groupby("customer_id")["power"].transform(
-            lambda x: x.rolling(24, min_periods=3).min()
-        )
+    df["power_std_24h"] = df.groupby("customer_id")["power"].transform(
+        lambda x: x.rolling(24, min_periods=3).std()
+    )
 
-        df["power_max_24h"] = df.groupby("customer_id")["power"].transform(
-            lambda x: x.rolling(24,min_periods=3).max()
-         )
-        
-        return df
+    df["power_min_24h"] = df.groupby("customer_id")["power"].transform(
+        lambda x: x.rolling(24, min_periods=3).min()
+    )
+
+    df["power_max_24h"] = df.groupby("customer_id")["power"].transform(
+        lambda x: x.rolling(24, min_periods=3).max()
+    )
+    
+    return df
